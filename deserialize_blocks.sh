698e70f6b8c85aacb29d4b2092781f097e96d17c
upalchowdhury <upal.chowdhury@outlook.com>
2021-03-31 11:55:31 -0400

    fix block script

--- deserialize_blocks.sh
+++ deserialize_blocks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BLOCKSTART=0
-BLOCKEND=640000
+BLOCKSTART=1000
+BLOCKEND=2000
 
 
-for ((i=$BLOCKSTART; i<$BLOCKEND;i+=100))
+for ((i=$BLOCKSTART; i<$BLOCKEND;i+=60))
 do
 
-	for ((j=i;j<i+100;j++))
+	for ((j=i;j<i+60;j++))
 	do
 		echo "Sending block $j"
 
@@ -18,7 +18,7 @@ do
 
 		FILENAME="block"${j}".json"
 
-		bitcoin-cli getblock ${BLOCKHASH} 2 | aws s3 cp - s3://CONFIG.s3blocks/${FILENAME} &
+		bitcoin-cli getblock ${BLOCKHASH} 2 | aws s3 cp - s3://blockdata/${FILENAME} &
 	done
 	wait
 done
